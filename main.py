f221c5d0c73a57b704a70d1ef1b0694f1e1ee92b
Eugene Moshchyn <eugene.moschyn@gmail.com>
2021-06-10 22:12:15 -0700

    Prettified the code and added docstrings.

--- main.py
+++ main.py
@@ -1,145 +1,172 @@
-#!/usr/bin/env python
-
 #Source of Morse is https://morsecode.scphillips.com/morse2.html
 
-
 import RPi.GPIO as GPIO
-import time
-import os
+from time import sleep
+from os import name as os_name
+from os import system
 
-# 'step'	= 1    					/signal on/off
-# 'dit'		= step					/dot
-# 'dash'	= 3 * step				/dash
-# 'pause'	= 3 * step 				/pause after letters
-# 'space'	= 7 * step				/pause between words
-# 
-#
-# Average number of letters for English word is 4.79
-# However, the most common range is [5,6]
+"""
+	'step'	= 1    				/signal on/off
+	'dit'	= step				/dot
+	'dash'	= 3 * step			/dash
+	'pause'	= 3 * step 			/pause after letters
+	'space'	= 7 * step			/pause between words
+"""
 
-non   = 'non'
-space = ' '
-pause = 0.2 #assign from user input
-tWork = 0.5 #assing from user input 
+# Base time for one symbol
+BASE_TIME = 0.5
+# Pin Number where LED is located
+# Refer to
+LED_PIN   = 11
 
-Led   = 11
-
-morse = [
-	[space],			# ' ' /32
-	['-.-.--'],			# !
-	['.-..-.'],			# "
-	[none],				# #
-	[none],				# $
-	[none],				# %	
-	['.-...'],			# & V
-	['.----.'],			# ' V
-	['-.--.'],			# (
-	['-.--.-'],			# )
-	[none],				# *
-	['.-.-.'],			# +
-	['--..--'],			# ,
-	['-....-'],			# -
-	['.-.-.-'],			# .
-	['-..-.'],			# /
-	['----'],			# 0 V
-	['.---'],			# 1 V
-	['..---'],			# 2 V
-	['...--'],			# 3 V
-	['....-'],			# 4 V
-	['.....'],			# 5 V
-	['-....'],			# 6 V
-	['--...'],			# 7 V
-	['---..'],			# 8 V
-	['----.'],			# 9 V
-	['---...'],			# :
-	[none],				# ;
-	[none],				# <
-	[none],				# >
-	['..--..'],			# ?
-	['.--.-.'],			# @ V
-    ['.-'],    			# A /65
-    ['-...'],    		# B
-    ['-.-.'],    		# C
-    ['-..'],			# D
-    ['.'],  			# E
-    ['..-.'],    		# F
-    ['--.'],    		# G
-    ['....'],    		# H
-    ['..'],    			# I
-    ['.---'],   		# J
-    ['-.-'],    		# K
-    ['.-..'],    		# L
-    ['--'],    			# M
-    ['-.'],    			# N
-    ['---'],    		# O
-    ['.--.'],    		# P
-    ['--.-'],   		# Q
-    ['.-.'],    		# R
-    ['...'],    		# S
-    ['-'],    			# T
-    ['..-'],    		# U
-    ['...-'],   		# V
-    ['.--'],    		# W
-    ['-..-'],   		# X
-    ['-.--'],   		# Y
-    ['--..']     		# Z
-]
+# Dictionary with all acceptable characters
+# New ones can be appended
+MORSE_CODE = {
+	' ':  ' ',
+	'!':  '-.-.--',
+	'"':  '.-..-.',
+	'&':  '.-...',
+	'\'': '.----.',
+	'(':  '-.--.',
+	')':  '-.--.-',
+	'+':  '.-.-.',
+	',':  '--..--',
+	'-':  '-....-',
+	'.':  '.-.-.-',
+	'/':  '-..-.',
+	'0':  '----',
+	'1':  '.---',
+	'2':  '..---',
+	'3':  '...--',
+	'4':  '....-',
+	'5':  '.....',
+	'6':  '-....',
+	'7':  '--...',
+	'8':  '---..',
+	'9':  '----.',
+	':':  '---...',
+	'?':  '..--..',
+	'@':  '.--.-.',
+    'A':  '.-',
+    'B':  '-...',
+    'C':  '-.-.',
+    'D':  '-..',
+    'E':  '.',
+    'F':  '..-.',
+    'G':  '--.',
+    'H':  '....',
+    'I':  '..',
+    'J':  '.---',
+    'K':  '-.-',
+    'L':  '.-..',
+    'M':  '--',
+    'N':  '-.',
+    'O':  '---',
+    'P':  '.--.',
+    'Q':  '--.-',
+    'R':  '.-.',
+    'S':  '...',
+    'T':  '-',
+    'U':  '..-',
+    'V':  '...-',
+    'W':  '.--',
+    'X':  '-..-',
+    'Y':  '-.--',
+    'Z':  '--..']
+}
 
 
 def setup():
+	"""
+		Default preparation for pins on Raspberry Pi
+		 - Setting up the 'LED_PIN'
+	"""
     GPIO.setmode(GPIO.BOARD)
-    GPIO.setup(Led, GPIO.OUT)
-    GPIO.output(Led, GPIO.HIGH)
-
+    GPIO.setup(LED_PIN,  GPIO.OUT)
+    GPIO.output(LED_PIN, GPIO.HIGH)
 
 
 def destroy():
-    GPIO.output(Led, GPIO.HIGH)
+	"""
+		Default cleanup for pins on Raspberry Pi
+		 - Removing any binds from 'LED_PIN'
+	"""
+    GPIO.output(LED_PIN, GPIO.HIGH)
     GPIO.cleanup()
 
-def outPut(raer):
-    for jo in range(4):
-        if (morse[raer][jo] == '.'):
-            print '...led on'
-            GPIO.output(Led, GPIO.LOW)
-            time.sleep(tWork)
-            os.system('clear')
-            print 'led off...'
-            GPIO.output(Led, GPIO.HIGH)
-            time.sleep(pause)
-            os.system('clear')
-        elif (morse[raer][jo] == '-'):
-            print '...led on'
-            GPIO.output(Led, GPIO.LOW)
-            time.sleep(3 * tWork)
-            print 'led off...'
-            GPIO.output(Led, GPIO.HIGH)
-            time.sleep(pause)
-        else:
-            break
-    #letter is done, pause for 0.5 seconds
-    time.sleep(1)
+
+def clear():
+	"""
+		Screen(Terminal/Command Line) cleanup based
+		on Operating System
+	"""
+	if os_name == 'nt':
+		system('cls')
+	else:
+		system('clear')
 
 
-def exe():
+def get_time_based_on_symbol(character: str):
+	"""
+		Returns the time when 'LED_PIN' should be on
+	"""
+	if (character == '.'):
+		return BASE_TIME
+
+	if (character == '-'):
+		return 3 * BASE_TIME
+
+	if (character == ' '):
+		return 7 * BASE_TIME
+
+	return 0
+
+
+def light_diode(on_time: float):
+	"""
+		Lights the diode at 'LED_PIN' for 'on_time' secs.
+	"""
+	GPIO.output(LED_PIN, GPIO.LOW)
+	sleep(on_time)
+	GPIO.output(LED_PIN, GPIO.HIGH)
+
+
+def display_process(string: str, character: str):
+	"""
+		Outputs the message for user
+	"""
+	clear()
+	print('Your phrase:')
+	print(string)
+	print('Letter being output:')
+	print(character)
+
+
+def output_letter(morse_code: str):
+	"""
+		If 'morse_code' output the symbols for given letter,
+		then wait 'BASE_TIME'(wait between letters)
+	"""
+	for character in morse_code:
+		on_time = get_time_based_on_symbol(character)
+		light_diode(on_time)
+		sleep(BASE_TIME)
+
+
+def main():
     while True:
-        os.system('clear')
-        print 'Enter the string w/o spaces'
-        inp = raw_input()
-        inp = inp.upper()
-        for i in range(len(inp)):
-            num = ord(inp[i]) - 65
-            if num >= 0 and num <= 25:
-                outPut(num)
-        
-        
+        clear()
+        print('Enter the phrase:')
+        string = input()
+        for character in string.upper():
+			display_process(string, character)
+            output_letter( MORSE_CODE.get(character, "") )
+
+
 
 if __name__ == '__main__':
     setup()
     try:
-        exe()
+        main()
     except KeyboardInterrupt:
         destroy()
-
-
-
